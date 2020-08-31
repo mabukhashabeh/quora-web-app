d15a33e2d74266a49f6a3319630927c7027f553d
mohammadabukhashabeh <mohammad.abukhashabeh97@gmail.com>
2020-08-31 22:02:26 +0300

    Add AnswerCreateAPIView & AnswerListAPIView

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -7,5 +7,13 @@ router = DefaultRouter()
 router.register(r"questions", views.QuestionViewSet)
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+
+    path("questions/<slug:question_slug>/answers/",
+         views.AnswerListAPIView.as_view(),
+         name="answer-list"),
+
+    path("questions/<slug:question_slug>/answer/",
+         views.AnswerCreateAPIView.as_view(),
+         name="create-answer")
 ]
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,14 +1,16 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 
 from apps.api.permissions import IsAuthorOrReadOnly
-from apps.api.serializers import QuestionSerializer
-from apps.question.models import Question
+from apps.api.serializers import QuestionSerializer, AnswerSerializer
+from apps.question.models import Question, Answer
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
     """
-    ModelViewSet provide full crud mechanism
+    ModelViewSet provide full crud
     for specific model
     """
     queryset = Question.objects.all()
@@ -18,3 +20,28 @@ class QuestionViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class AnswerCreateAPIView(generics.CreateAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        question_slug = self.kwargs.get("question_slug")
+        question = get_object_or_404(Question, slug=question_slug)
+
+        if question.answers.filter(author=user).exists():
+            raise ValidationError("You have already answered this Question!")
+
+        serializer.save(author=user, question=question)
+
+
+class AnswerListAPIView(generics.ListAPIView):
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        question_slug = self.kwargs.get("slug")
+        return Answer.objects.filter(question__slug=question_slug)
